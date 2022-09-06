c2f79a6f01411a2cb4d600eb055a3b1e17f38d37
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-09-06 22:50:01 +0300

    Task64

---
+++ Task64.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Natural(int N) {
+    if (N == 0) return;
+    Console.Write($"{N} ");
+    Natural(N - 1);
+}
+Natural(n);
